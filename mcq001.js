131763aeca1030f9256912127f84af1d9ace8ab4
Frederic-vW <fvw.github@gmail.com>
2023-01-18 08:32:10 +1100

    Update mcq001.js

--- mcq001.js
+++ mcq001.js
@@ -1,140 +1,142 @@
-//vb = document.getElementById("video_box")
-//vb = document.getElementsByClassName("book_content")[0]
-vb = document.getElementsByClassName("box py-3 generalbox book_content")[0]
-vrect = vb.getBoundingClientRect()
-vs = document.getElementsByTagName('video')
-v = vs[0]
-if (v.getAttribute('title') == 'Troemner_1.mp4') {
-    v.ontimeupdate = function() {showQuestion()};
-    v.volume = 0.0;
-}
-// main question div qd
-qd = document.createElement("div")
-qd.className = "modal"
-//
-Object.assign(qd.style, {
-    display: "none",
-    position: "absolute",
-    overflow: "auto",
-    backgroundColor: "rgba(200,200,200,0.7)",
-    left: vrect.left,
-    top: vrect.top,
-    width: vrect.width,
-    height: vrect.height,
-})
-//
-/*
-Object.assign(qd.style, {
-    display: "none",
-    position: "absolute",
-    overflow: "auto",
-    backgroundColor: "rgba(200,200,200,0.7)",
-    left: "25%",
-    top: "25%",
-    width: "100%",
-    height: "100%",
-})
-*/
-qd2 = document.createElement("div")
-qd2.style.cssText = `position: relative; padding: 20px; margin: auto;
-width: 100%; background-color: rgba(250,250,250,0.9);`
-h1 = document.createElement("h1")
-h1.innerHTML = "Quick check"
-qd2.appendChild(h1)
+$(window).on('load', function() {
+    //vb = document.getElementById("video_box")
+    //vb = document.getElementsByClassName("book_content")[0]
+    vb = document.getElementsByClassName("box py-3 generalbox book_content")[0]
+    vrect = vb.getBoundingClientRect()
+    vs = document.getElementsByTagName('video')
+    v = vs[0]
+    if (v.getAttribute('title') == 'Troemner_1.mp4') {
+        v.ontimeupdate = function() {showQuestion()};
+        v.volume = 0.0;
+    }
+    // main question div qd
+    qd = document.createElement("div")
+    qd.className = "modal"
+    //
+    Object.assign(qd.style, {
+        display: "none",
+        position: "absolute",
+        overflow: "auto",
+        backgroundColor: "rgba(200,200,200,0.7)",
+        left: vrect.left,
+        top: vrect.top,
+        width: vrect.width,
+        height: vrect.height,
+    })
+    //
+    /*
+    Object.assign(qd.style, {
+        display: "none",
+        position: "absolute",
+        overflow: "auto",
+        backgroundColor: "rgba(200,200,200,0.7)",
+        left: "25%",
+        top: "25%",
+        width: "100%",
+        height: "100%",
+    })
+    */
+    qd2 = document.createElement("div")
+    qd2.style.cssText = `position: relative; padding: 20px; margin: auto;
+    width: 100%; background-color: rgba(250,250,250,0.9);`
+    h1 = document.createElement("h1")
+    h1.innerHTML = "Quick check"
+    qd2.appendChild(h1)
 
-p1 = document.createElement("p")
-p1.innerHTML = "Which statements are correct?"
-qd2.appendChild(p1)
+    p1 = document.createElement("p")
+    p1.innerHTML = "Which statements are correct?"
+    qd2.appendChild(p1)
 
-p2 = document.createElement("p")
+    p2 = document.createElement("p")
 
-// answer (A)
-dA = document.createElement("div")
-dA.setAttribute("style", "display: inline-block;")
-iA = document.createElement("input")
-Object.assign(iA, {type: "checkbox"})
-lA = document.createElement("label")
-lA.setAttribute("for", "A")
-lA.innerHTML = "True"
-fbA = document.createElement("div")
-Object.assign(fbA.style, {color: "blue", fontStyle: "italic"})
-dA.appendChild(iA)
-dA.appendChild(lA)
-dA.appendChild(fbA)
+    // answer (A)
+    dA = document.createElement("div")
+    dA.setAttribute("style", "display: inline-block;")
+    iA = document.createElement("input")
+    Object.assign(iA, {type: "checkbox"})
+    lA = document.createElement("label")
+    lA.setAttribute("for", "A")
+    lA.innerHTML = "True"
+    fbA = document.createElement("div")
+    Object.assign(fbA.style, {color: "blue", fontStyle: "italic"})
+    dA.appendChild(iA)
+    dA.appendChild(lA)
+    dA.appendChild(fbA)
 
-// answer (B)
-dB = document.createElement("div")
-iB = document.createElement("input")
-Object.assign(iB, {type: "checkbox"})
-lB = document.createElement("label")
-lB.setAttribute("for", "B")
-lB.innerHTML = "False"
-fbB = document.createElement("div")
-Object.assign(fbB.style, {color: "blue", fontStyle: "italic"})
-dB.appendChild(iB)
-dB.appendChild(lB)
-dB.appendChild(fbB)
+    // answer (B)
+    dB = document.createElement("div")
+    iB = document.createElement("input")
+    Object.assign(iB, {type: "checkbox"})
+    lB = document.createElement("label")
+    lB.setAttribute("for", "B")
+    lB.innerHTML = "False"
+    fbB = document.createElement("div")
+    Object.assign(fbB.style, {color: "blue", fontStyle: "italic"})
+    dB.appendChild(iB)
+    dB.appendChild(lB)
+    dB.appendChild(fbB)
 
-// answer (C)
-dC = document.createElement("div")
-iC = document.createElement("input")
-Object.assign(iC, {type: "checkbox"})
-lC = document.createElement("label")
-lC.setAttribute("for", "C")
-lC.innerHTML = "Maybe"
-fbC = document.createElement("div")
-Object.assign(fbC.style, {color: "blue", fontStyle: "italic"})
-dC.appendChild(iC)
-dC.appendChild(lC)
-dC.appendChild(fbC)
+    // answer (C)
+    dC = document.createElement("div")
+    iC = document.createElement("input")
+    Object.assign(iC, {type: "checkbox"})
+    lC = document.createElement("label")
+    lC.setAttribute("for", "C")
+    lC.innerHTML = "Maybe"
+    fbC = document.createElement("div")
+    Object.assign(fbC.style, {color: "blue", fontStyle: "italic"})
+    dC.appendChild(iC)
+    dC.appendChild(lC)
+    dC.appendChild(fbC)
 
-// answer (D)
-dD = document.createElement("div")
-iD = document.createElement("input")
-Object.assign(iD, {type: "checkbox"})
-lD = document.createElement("label")
-lD.setAttribute("for", "D")
-lD.innerHTML = "No clue..."
-fbD = document.createElement("div")
-Object.assign(fbD.style, {color: "blue", fontStyle: "italic"})
-dD.appendChild(iD)
-dD.appendChild(lD)
-dD.appendChild(fbD)
+    // answer (D)
+    dD = document.createElement("div")
+    iD = document.createElement("input")
+    Object.assign(iD, {type: "checkbox"})
+    lD = document.createElement("label")
+    lD.setAttribute("for", "D")
+    lD.innerHTML = "No clue..."
+    fbD = document.createElement("div")
+    Object.assign(fbD.style, {color: "blue", fontStyle: "italic"})
+    dD.appendChild(iD)
+    dD.appendChild(lD)
+    dD.appendChild(fbD)
 
-// put elements in order
-p2.appendChild(dA)
-p2.appendChild(dB)
-p2.appendChild(dC)
-p2.appendChild(dD)
+    // put elements in order
+    p2.appendChild(dA)
+    p2.appendChild(dB)
+    p2.appendChild(dC)
+    p2.appendChild(dD)
 
-qd2.appendChild(p2)
+    qd2.appendChild(p2)
 
-p3 = document.createElement("p")
-b1 = document.createElement("button")
-b1.setAttribute("id", "submit-button")
-b1.onclick = function(){
-    // submit function
-    fbA.innerHTML = get_feedback(iA, "A")
-    fbB.innerHTML = get_feedback(iB, "B")
-    fbC.innerHTML = get_feedback(iC, "C")
-    fbD.innerHTML = get_feedback(iD, "D")
-    b2.innerHTML = "Continue"
-}
-b1.innerHTML = "Submit"
-p3.appendChild(b1)
+    p3 = document.createElement("p")
+    b1 = document.createElement("button")
+    b1.setAttribute("id", "submit-button")
+    b1.onclick = function(){
+        // submit function
+        fbA.innerHTML = get_feedback(iA, "A")
+        fbB.innerHTML = get_feedback(iB, "B")
+        fbC.innerHTML = get_feedback(iC, "C")
+        fbD.innerHTML = get_feedback(iD, "D")
+        b2.innerHTML = "Continue"
+    }
+    b1.innerHTML = "Submit"
+    p3.appendChild(b1)
 
-b2 = document.createElement("button")
-b2.setAttribute("id", "skip-button")
-b2.onclick = function(){
-    qd.style.display = "none";
-    v.play();
-}
-b2.innerHTML = "Skip"
-p3.appendChild(b2)
-qd2.appendChild(p3)
-qd.appendChild(qd2)
-vb.appendChild(qd)
-//document.appendChild(qd)
+    b2 = document.createElement("button")
+    b2.setAttribute("id", "skip-button")
+    b2.onclick = function(){
+        qd.style.display = "none";
+        v.play();
+    }
+    b2.innerHTML = "Skip"
+    p3.appendChild(b2)
+    qd2.appendChild(p3)
+    qd.appendChild(qd2)
+    vb.appendChild(qd)
+    //document.appendChild(qd)
+});
 
 var q1_shown = false;
 function showQuestion() {
